51c522f64cac9a218eacd503617a9db00ef33209
Adarsh Hatkar <80952515+AdarshHatkar@users.noreply.github.com>
2026-03-01 15:24:41 +0530

    export updated

--- dev.md
+++ dev.md
@@ -1,1 +1,1 @@
-"@digicroz/js-kit": "file:C:/digicroz-repos/npm-packages/js-kit",
+"@digicroz/filestore-server": "file:C:/digicroz-repos/npm-packages/filestore-server",
---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import { dcFileStoreS2S, dcFileStoreZodSchemas } from "./helpers/s2s/dcFileStoreS2S.js";
+
+export type { TDcFileStoreInput, TDcFileStoreOutput } from "./helpers/s2s/dcFileStoreS2S.js";
+export { dcFileStoreZodSchemas } from "./helpers/s2s/dcFileStoreS2S.js";
+
+const s2sClient = dcFileStoreS2S();
+
+export const getPathInfo = s2sClient.fsAccess.getPathInfo.query;
+export const requestUploadUrl = s2sClient.fsAccess.requestUploadUrl.mutate;
+export const confirmUpload = s2sClient.fsAccess.confirmUpload.mutate;
+export const deleteFile = s2sClient.fsAccess.delete.mutate;
